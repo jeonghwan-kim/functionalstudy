87428176b16ec3cde757c6d594f7def1aed2f974
jeonghwan-kim <ej88ej@gmail.com>
2016-10-26 10:25:21 +0900

    Chapter 07 > begin

---
+++ src/07.spec.js
@@ -0,0 +1,20 @@
+const _ = require('underscore');
+const should = require('should');
+const util = require('./util');
+const _01 = require('./01');
+const _02 = require('./02');
+const _03 = require('./03');
+const _04 = require('./04');
+const _05 = require('./05');
+const _06 = require('./06');
+const _07 = require('./07');
+
+describe('07.js', () => {
+  describe('randString()', () => {
+    it.only('', () => {
+      // _07.randString(0).should.be.equal('')
+      console.log(_07.randString(10))
+    })
+
+  });
+});
