446ca6cffba9675f461f068daf096a306b425185
jeonghwan-kim <ej88ej@gmail.com>
2016-11-14 09:35:02 +0900

    homework1

---
+++ src/homework.spec.js
@@ -0,0 +1,19 @@
+const should = require('should');
+const homework = require('./homework');
+
+describe('homework', () => {
+  describe.only('solution1', () => {
+    it('should return sum value', () => {
+      [1, 2, 3, 4].reduce(homework.solution1).should.be.equal(10);
+      [1, 2, 3, 4, 5, 6, 7, 8, 9].reduce(homework.solution1).should.be.equal(45);
+    });
+    it('should check parameters', () => {
+      (() => ['1', 2].reduce(homework.solution1)).should.throw();
+      (() => [1, '2'].reduce(homework.solution1)).should.throw();
+      (() => ['1', '2'].reduce(homework.solution1)).should.throw();
+    });
+    it('should check result', () => {
+      (() => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].reduce(homework.solution1)).should.throw();
+    });
+  });
+});
